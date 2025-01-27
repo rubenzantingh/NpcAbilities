1e818d97e5cf2594fffb9b33091c2f7b0cc100ff
Ruben Zantingh <ruben@e-tales.nl>
2025-01-27 21:17:23 +0100

    Add hotkey modes, hold and toggle

--- NpcAbilities.lua
+++ NpcAbilities.lua
@@ -1,6 +1,6 @@
-
 local npcAbilitiesFrame = CreateFrame("Frame")
 local hotkeyButtonPressed = false
+local checkForHotkeyReleased = false
 
 local function GetDataByID(dataType, dataId)
     data = _G[dataType]
@@ -92,9 +92,32 @@ local function SetNpcAbilityData()
     end
 end
 
+local function CheckHotkeyState()
+    local hotkey = NpcAbilitiesOptions["SELECTED_HOTKEY"]
+
+    if not IsKeyDown(hotkey) then
+        checkForHotkeyReleased = false
+        hotkeyButtonPressed = false
+        GameTooltip:SetUnit("mouseover");
+        npcAbilitiesFrame:SetScript("OnUpdate", nil)
+    end
+end
+
+local function StartCheckingHotkey()
+    checkForHotkeyReleased = true
+
+    npcAbilitiesFrame:SetScript("OnUpdate", function(self, elapsed)
+        CheckHotkeyState()
+    end)
+end
+
 local function SetHotkeyButtonPressed(self, key, eventType)
    if NpcAbilitiesOptions["SELECTED_HOTKEY"] then
        if eventType == "OnKeyDown" and key == NpcAbilitiesOptions["SELECTED_HOTKEY"] then
+           if NpcAbilitiesOptions["SELECTED_HOTKEY_MODE"] == "hold" then
+               StartCheckingHotkey()
+           end
+           
             if hotkeyButtonPressed then
                hotkeyButtonPressed = false
             else
